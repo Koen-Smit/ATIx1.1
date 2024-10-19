9fbb643d79a8549aaaf7cc3c08cfd5ec528d5b51
KerrieMeister96 <koensmit13@gmail.com>
2024-10-19 23:58:02 +0200

    changes

--- SmartEnergy.Client/obj/Debug/net8.0/SmartEnergy.Client.AssemblyInfo.cs
+++ SmartEnergy.Client/obj/Debug/net8.0/SmartEnergy.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartEnergy.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44f0ac7354cd5d47e7039b67ea126723b580bc07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd0fc5a823b4c547ea73ce556ca618da25cc7a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartEnergy.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartEnergy.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
